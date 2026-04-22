e1db3bf710f06f2d6bd7d49989f8932fc1f9ee06
nogurtMon <montierthn@gmail.com>
2026-04-22 16:45:10 -0700

    ahhh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 KEY_FILE=/data/dsn-key
 
-if [ -z "$DSN_ENCRYPTION_KEY" ]; then
-  if [ -f "$KEY_FILE" ]; then
-    export DSN_ENCRYPTION_KEY=$(cat "$KEY_FILE")
-  else
-    export DSN_ENCRYPTION_KEY=$(openssl rand -hex 32)
-    mkdir -p /data
-    echo "$DSN_ENCRYPTION_KEY" > "$KEY_FILE"
-  fi
+if [ -f "$KEY_FILE" ]; then
+  export DSN_ENCRYPTION_KEY=$(cat "$KEY_FILE")
+elif [ -z "$DSN_ENCRYPTION_KEY" ]; then
+  export DSN_ENCRYPTION_KEY=$(openssl rand -hex 32)
+  mkdir -p /data
+  echo "$DSN_ENCRYPTION_KEY" > "$KEY_FILE"
 fi
 
 exec "$@"
